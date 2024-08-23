7bd8d26f900f68f1a026812e6089242bd7f7609d
tedhun21 <songjihun940409@gmail.com>
2024-08-23 10:37:33 +0900

    chore: Home directory git error

---
+++ src/pages/main/api.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { getCookie } from 'utils/cookie';
+
+interface IPagination {
+  page: number;
+  size: number;
+}
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export async function getFavoritePetsitters({ page, size }: IPagination) {
+  const access_token = getCookie('access_token');
+  if (access_token) {
+    try {
+      const { data } = await axios.get(`${API_URL}/user/favorite?page=${page}&size=${size}`, {
+        headers: { Authorization: `Bearer ${access_token}` },
+      });
+
+      return data;
+    } catch (error) {}
+  }
+}
+
+export async function getNewestReview({ page, size }: IPagination) {
+  try {
+    const { data } = await axios.get(`${API_URL}/reviews?page=${page}&size=${size}`);
+    return data;
+  } catch (e) {}
+}
